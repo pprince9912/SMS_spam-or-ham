c1b61fe3976df1322a6622ad8afe3e74491fb424
praiseprince <praiseprince35@gmail.com>
2024-07-29 17:54:16 -0400

    commit3

--- website/model.py
+++ website/model.py
@@ -1,16 +1,8 @@
 import numpy as np
-import pandas as pd
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 import string
-import pandas as pd
-
-def preprocess_sms(df):
-    df = df.sample(frac = 1, ignore_index = True, random_state = 42)
-    X = df.text
-    Y = df.spam.to_numpy()
-
-    return X, Y
+import pickle
 
 def preprocess_text(X):
     stop = set(stopwords.words('english') + list(string.punctuation))
@@ -28,26 +20,6 @@ def preprocess_text(X):
         return X_preprocessed[0]
     
     return X_preprocessed
-
-def get_word_frequency(X,Y):
-    word_dict = {}
-    num_sms = len(X)
-
-    for i in range(num_sms):
-        sms = X[i] 
-        cls = Y[i] 
-        sms = set(sms) 
-
-        for word in sms:
-            if word not in word_dict.keys():
-                word_dict[word] = {"spam": 1, "ham": 1}
-
-            if cls == 0:    
-                word_dict[word]["ham"] += 1
-            if cls == 1:
-                word_dict[word]["spam"] += 1
-
-    return word_dict
 
 def prob_word_given_class(word, cls, word_frequency, class_frequency):
     amount_word_and_class = word_frequency[word][cls]
@@ -91,17 +63,5 @@ def log_naive_bayes(treated_sms, word_frequency, class_frequency, return_likelih
     else:
         return 0
 
-
-dataframe_sms = pd.read_csv('dataset\sms.csv')
-dataframe_sms['spam'] = dataframe_sms['spam'].map({'spam': 1, 'ham': 0})
-
-X, Y = preprocess_sms(dataframe_sms)
-X_treated = preprocess_text(X)
-TRAIN_SIZE = int(0.80*len(X_treated)) 
-
-X_train = X_treated[:TRAIN_SIZE]
-Y_train = Y[:TRAIN_SIZE]
-    
-word_frequency = get_word_frequency(X_train,Y_train)
-class_frequency = {'ham': sum(Y_train == 0), 'spam': sum(Y_train == 1)}
-proportion_spam = class_frequency['spam']/(class_frequency['ham'] + class_frequency['spam'])
+with open('parameters.pkl', 'rb') as f:
+    word_frequency, class_frequency = pickle.load(f)
